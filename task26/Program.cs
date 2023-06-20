b3b1525424a5df7cd12d4136d037ec9d20cdc335
Artem Tsytsylin <workhardgofock@gmail.com>
2023-06-20 21:13:50 +0300

    26 is done

---
+++ task26/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Input(){
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+return num;
+}
+int calc(int a){
+int numOfNum = 0;
+while (a > 0){
+    a /= 10;
+    numOfNum++;
+}
+return numOfNum;
+}
+int number = Input();
+int col = calc(number);
+Console.WriteLine(col);
