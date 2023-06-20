e06f1a621f30fefa8cacab8cd5bb8bb483624a9d
Artem Tsytsylin <workhardgofock@gmail.com>
2023-06-20 20:51:09 +0300

    task24 is done

---
+++ task24/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+int ReadInt(){
+
+Console.WriteLine("Введите целое число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+return value;
+}
+
+int SumFrom1ToA(int a){
+
+    int sum = 0;
+    for(int i = 1; i <= a; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+int number = ReadInt();
+int summa = SumFrom1ToA(number);
+Console.WriteLine($"Cумма чисел от 1 до {number} = {summa}");
