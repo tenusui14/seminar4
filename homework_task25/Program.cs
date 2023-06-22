6e30561db0da13805ec7278bf6715eff5a2ee186
Artem Tsytsylin <workhardgofock@gmail.com>
2023-06-22 18:14:27 +0300

    homework task 25 done

---
+++ homework_task25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B. НЕ ИСПОЛЬЗОВАТЬ MATH.POW()
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int temp = numberA;
+while (i < numberB){
+    numberA *= temp;
+    i++;
+}
+Console.WriteLine($"{temp} в степени {numberB} = {numberA}");
